da67bbeba0bcafd40c3f3638f5dca362bb8839e7
Esther Ataíde <144352675+ataideesther@users.noreply.github.com>
2024-08-26 11:55:49 -0300

    Add files via upload

---
+++ atividade charles/algoritmo.js
@@ -0,0 +1,8 @@
+var n1 =document.querySelector('#n1')
+var n2 =document.querySelector('#n2')
+var resultado = document.querySelector ('#resultado');
+
+
+function multiplicar(){
+        resultado.innerHTML = parseFloat(n1.value) * parseFloat(n2.value)
+    }
